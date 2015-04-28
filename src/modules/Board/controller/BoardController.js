54fd2e72ddd7fe76f010d21a612924bf4b80f53a
anthonyptetlow <anthonyptetlow@gmail.com>
2015-04-28 23:19:20 +0100

    Creating the card UI

--- src/modules/Board/controller/BoardController.js
+++ src/modules/Board/controller/BoardController.js
@@ -1,11 +1,50 @@
 angular.module('RedmineBoard').controller('BoardController', [
 	'$http',
-	function ($http) {
-		var key = localStorage.getItem('apiKey');
-		$http.get('http://redmine.carnyx.com/issues.json?key=' + key + '&assigned_to_id=130').success(function (data) {
-			console.log(data);
-		}).error(function(error){
-			console.log(error);
+	'$filter',
+	function ($http, $filter) {
+		var Board = this;
+		Board.key = localStorage.getItem('apiKey');
+
+
+
+		function filterToDo (issue) {
+			return !(filterInProgress(issue) || filterAwaiting(issue));
+		}
+
+		function filterInProgress(issue) {
+			return issue.status.name === 'In Progress';
+		}
+
+		function filterAwaiting(issue) {
+			return issue.status.name === "Awaiting user response";
+		}
+
+		Board.columns = [
+			{
+				name: 'Todo',
+				filterFn: filterToDo
+			},
+			{
+				name: 'In Progress',
+				filterFn: filterInProgress
+			},
+			{
+				name: 'Awaiting User Response',
+				filterFn: filterAwaiting
+			}
+		];
+
+		$http.get('/api/issues?key=' + Board.key + '&user=130').success(function (data) {
+
+			Board.issues = data.issues;
+			console.log(data.issues);
+
+		}).error(function(){
+			Board.errorMessage = 'Unable to Load Issues';
 		});
+
+		Board.getName = function(issue) {
+			return issue.priority.id;
+		};
 	}
 ]);
---
+++ src/modules/Board/directive/IssueCard.js
@@ -0,0 +1,32 @@
+angular.module('RedmineBoard').directive('issueCard', [
+	function(){
+		return {
+			scope: {
+				issue: '='
+			}, // {} = isolate, true = child, false/undefined = no change
+			controller: function($scope) {//, $element, $attrs, $transclude) {
+				var Issue = this;
+				function getPriorityClass(priorityNo) {
+					if (priorityNo >= 6) {
+						return 'imp-priority';
+					} else if (priorityNo >= 5) {
+						return 'high-priority';
+					} else if (priorityNo === 4) {
+						return 'med-priority';
+					}
+					return 'low-priority';
+				}
+
+				Issue.classes = getPriorityClass($scope.issue.priority.id);
+			},
+			controllerAs: 'Issue',
+			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+			restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+			templateUrl: './modules/Board/partials/IssueCard.html',
+			// replace: true,
+			// transclude: true,
+			// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+			link: function() {}
+		};
+	}
+]);
